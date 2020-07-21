790bda7c0c9894cc31de4843263d69aad18b3863
Ramanpreet Kaur <dhamiraman36@gmail.com>
2020-07-21 13:23:26 -0700

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -18,6 +19,9 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+const secret = "Thisiourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encrypted:["password"]});
 
 const User = mongoose.model("User", userSchema);
 
